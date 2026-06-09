245a1f7387d44101b3818f3a3e0b36488ad463c9
lafraiarafael <rafaellafraia@gmail.com>
2026-06-09 06:43:08 +0200

    feat: add advanced questions to active pools

---
+++ question-pool-enhancer.js
@@ -0,0 +1,4 @@
+(function enhanceQuestionPools() {
+  window.EXTRA_QUESTIONS = (window.EXTRA_QUESTIONS || []).concat(window.ADVANCED_QUESTIONS || []);
+  window.EXTRA_QUESTIONS_NL = (window.EXTRA_QUESTIONS_NL || []).concat(window.ADVANCED_QUESTIONS_NL || []);
+})();
